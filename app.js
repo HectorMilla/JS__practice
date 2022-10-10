26425b6838fa8596b0a8b448319f34842bec7e2a
hector galicia milla <hectormilla8@gmail.com>
2022-10-10 08:36:28 -0400

    added remove task function/event

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// defien UI Vars
+// define UI Vars
 
 const form= document.querySelector('#task-form')
 const taskList= document.querySelector('.collection')
@@ -16,6 +16,8 @@ loadEventListeners()
 function loadEventListeners(){
   // Add task event
   form.addEventListener('submit', addTask)
+  // Remove task event
+  taskList.addEventListener('click', removeTask)
 }
 
 // Add task
@@ -24,4 +26,38 @@ function addTask(e){
   if(taskInput.value === '') {
     alert('add task')
   }
+
+  // create li element
+  
+  const li = document.createElement('li')
+  li.className = 'collection-item'
+  li.appendChild(document.createTextNode(taskInput.value))
+
+  // create new link element
+  const link = document.createElement('a')
+  link.className = 'delete-item secondary-content'
+  // add icon html
+  link.innerHTML = '<i class = "fa fa-remove"></i>'
+  li.appendChild(link)
+  // append to task list
+  taskList.appendChild(li)
+
+  // Clear input
+  taskInput.value = ''
+  
+  e.preventDefault()
+}
+
+// Remove Task
+
+function removeTask (e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')){
+      listItem = e.target.parentElement.parentElement
+      listItem.remove()
+    }
+   
+
+  }
+  console.log(e.target.className)
 }
